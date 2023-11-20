2efa529150153c0b42d778cff0f0d931e44d9696
nahom-ketsela <nahoket@gmail.com>
2023-11-20 16:17:13 +0300

    first commit

--- firstprogram.java
+++ firstprogram.java
@@ -1,14 +1,6 @@
 public class firstprogram {
     public static void main ( String[] args) {
-          System.out.println( "Welcome to java");
-          String text1, text2;
-          text1= "coffee";
-          text2= "I Love Java and Java Loves me";
-          System.out.println(text1.toUpperCase());
-                    System.out.println(text1 +" "+ text2);
-                    System.out.println(text2.length());
-                    System.out.println(text2.indexOf("L"));
-
-
-       }
+          System.out.println( "Hello world");
+       
+           }
    }
