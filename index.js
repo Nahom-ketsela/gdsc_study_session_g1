2e2ba14c64c57f8f9ae2cdbb4504f451aa2c3e17
nahom-ketsela <nahoket@gmail.com>
2023-11-29 13:05:45 +0300

    second commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// console.log(a);
+// var a=25;
+// console.log(a);
+// var num1,num2;
+// num1=2;
+// num2=44;
+// console.log(num1);
+// console.log(num2);
+
+
+// let age=43;
+// console.log(age);
+const mass = prompt ("what is your weight? ")
+const height = prompt ("what is your height? ")
+const BMI = mass/(height*height);
+console.log(BMI);
+
